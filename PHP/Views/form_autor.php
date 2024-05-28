23b30082740868dfed1ca60f4660edf862bb8675
JoãoGSilveira <joaosilveira31@hotmail.com>
2024-05-28 08:50:21 -0300

    Update PHP

--- PHP/Views/form_autor.php
+++ PHP/Views/form_autor.php
@@ -29,8 +29,35 @@ if($_POST){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro Autor</title>
+    <style>
+        .navbar {
+            background-color: #333;
+            overflow: hidden;
+        }
+        
+        .navbar a {
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 20px;
+            text-decoration: none;
+        }
+        
+        .navbar a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+    </style>
 </head>
 <body>
+<div class="navbar">
+        <a href="index.php">Home</a>
+        <a href="form_editora.php">Cadastro Editoras</a>
+        <a href="form_autor.php">Cadastro Autores</a>
+        <a href="listar_autores.php">Listar Autores</a>
+        <a href="listar_editoras.php">Listar Editoras</a>
+    </div>
     <form action = "#" method = "post">
         <label>Nome Autor</label>
         <input type="text" name="nome" id="nome" value = "<?php echo isset($_POST['nome'])?$_POST['nome']:''?>">
